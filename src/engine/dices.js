d9fef71a3ad3a1b5374c763e54f6e1b49aabed3f
Iñaki Agudo <agudovitoria@gmail.com>
2021-09-08 12:41:09 +0200

    Create dice roller

--- src/engine/dices.js
+++ src/engine/dices.js
@@ -1,3 +1,5 @@
+import _ from 'underscore';
+
 export const D4 = 4;
 export const D6 = 6;
 export const D8 = 8;
@@ -5,3 +7,5 @@ export const D10 = 10;
 export const D12 = 12;
 export const D20 = 20;
 export const D100 = 100;
+
+export const rollDice = dice => _.random(0, dice);
